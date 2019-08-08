59d68e37a42136d7e24da2c3b48e4e0dc27a6881
Sim Yong Seng <simyseng@gmail.com>
2019-08-08 21:36:17 +0700

    First commit

---
+++ SecondApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SecondApp
+//
+//  Created by Sim Yong Seng on 8/8/19.
+//  Copyright © 2019 Sim Yong Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func printConsole(_ sender: Any) {
+        print("Hello world")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
